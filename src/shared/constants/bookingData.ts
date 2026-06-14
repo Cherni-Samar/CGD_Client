8cdaff3af2ee1a52cca0c7b91bcda7d54e15c900
samar <chernisamar029@gmail.com>
2026-06-14 19:12:43 +0100

    separation des constantes ailleurs

---
+++ src/shared/constants/bookingData.ts
@@ -0,0 +1,27 @@
+
+export const cities = [
+  'Los Angeles',
+  'Orange County',
+  'San Diego',
+  'Long Beach',
+  'Pasadena'
+];
+
+export const vehicles = [
+  'Sedan',
+  'SUV / Crossover',
+  'Truck',
+  'Sports Car',
+  'Luxury / Exotic'
+];
+
+export const servicesList = [
+  'Exterior Detail',
+  'Interior Detail',
+  'Full Detail',
+  'Ceramic Coating',
+  'Paint Correction',
+  'Golden Package',
+  'Fleet Services'
+];
+
